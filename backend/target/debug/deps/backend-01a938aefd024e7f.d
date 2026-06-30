6abf2fcd54f343ff30716c73b5b60af5cc160016
BlueCloudyRiver <bluecloudyriver@gmail.com>
2026-06-30 19:02:49 +0530

    start rust backend

---
+++ backend/target/debug/deps/backend-01a938aefd024e7f.d
@@ -0,0 +1,5 @@
+E:\Moon\Lab\Stratum\backend\target\debug\deps\backend-01a938aefd024e7f.d: src\main.rs
+
+E:\Moon\Lab\Stratum\backend\target\debug\deps\libbackend-01a938aefd024e7f.rmeta: src\main.rs
+
+src\main.rs:
---
+++ backend/target/debug/deps/backend-9bce7a0b8ac75070.d
@@ -0,0 +1,5 @@
+E:\Moon\Lab\Stratum\backend\target\debug\deps\backend-9bce7a0b8ac75070.d: src\main.rs
+
+E:\Moon\Lab\Stratum\backend\target\debug\deps\libbackend-9bce7a0b8ac75070.rmeta: src\main.rs
+
+src\main.rs:
