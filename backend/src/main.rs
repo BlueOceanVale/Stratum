792b8a952e07a09350cc23d070f858fe6a432f4f
BlueCloudyRiver <bluecloudyriver@gmail.com>
2026-07-12 21:33:05 +0530

    add password hashing with argon2

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,6 +3,11 @@ use tokio::net::TcpListener;
 use serde::Deserialize;
 use sqlx::PgPool;
 use dotenvy;
+use rand_core;
+use argon2::{
+    Argon2, PasswordHasher, password_hash::{Error, SaltString},
+};
+
 
 #[derive(Clone)]
 struct AppState {
@@ -14,6 +19,18 @@ struct RegisterRequest {
     name: String,
     email: String,
     password: String,
+}
+
+fn hash_password(password: &str) -> Result<String, Error> {
+    let argon2 = Argon2::default();
+
+    let salt = SaltString::generate(&mut rand_core::OsRng);
+
+    let password_hash = argon2.hash_password(password.as_bytes(), &salt);
+    match password_hash {
+        Ok(hash) => Ok(hash.to_string()),
+        Err(err) => Err(err),
+    }
 }
 
 #[tokio::main]
@@ -55,13 +72,22 @@ async fn register(
     println!("Username: {}", payload.name);
     println!("Email: {}", payload.email);
 
+    let hashed_password = hash_password(&payload.password);
+
+    let hashed_password = match hashed_password {
+        Ok(value) => value,
+        Err(err) => {
+            println!("{err}");
+            return "Failed to hash password"
+        }
+    };
 
     let result = sqlx::query("
         INSERT INTO users(name, email, password_hash)
         VALUES($1, $2, $3)
     ")  .bind(payload.name)
         .bind(payload.email)
-        .bind(payload.password)
+        .bind(hashed_password)
         .execute(&state.pool)
         .await;
 
@@ -73,4 +99,4 @@ async fn register(
         }
     }
 
-}
+} 
